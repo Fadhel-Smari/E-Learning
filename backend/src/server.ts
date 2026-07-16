959cc958cb2349ee8e833becef77d2cc6e3a3bc1
Gabriel Cadieux <gabrielcadieux96@gmail.com>
2026-07-16 17:33:04 -0400

    Ajout des routes auth et cours dans server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import coursRoutes from "./routes/cours.routes.js";
 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/cours", coursRoutes);
 
 const PORT = process.env.PORT || 3000;
 
