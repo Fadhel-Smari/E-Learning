5befba1a9a974a7ab0e26243a303ce0567964413
Gabriel Cadieux <gabrielcadieux96@gmail.com>
2026-08-27 00:53:40 -0400

    Ajout du components Navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+import '../App.css';
+
+export function Navbar() {
+  const { token, seDeconnecter } = useAuth();
+
+  return (
+    <header className="en-tete-navigation">
+      <nav>
+        {/* À Gauche */}
+        <Link to="/" className="accueil-lien">Mon E-Learning</Link>
+        
+        {/* À Droite */}
+        <div className="nav-groupe-liens">
+          {token ? (
+            <>
+              {/* <Link to="/cours">Mes Cours</Link> */}
+              <button onClick={seDeconnecter} className="bouton-lien">
+                Déconnexion
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/connexion">Connexion</Link>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+}
